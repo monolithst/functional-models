05a737f24b76435145cb83e4ef9e504953160654
Michael Cornwell <michael.a.cornwell@gmail.com>
2021-12-02 16:09:38 -0500

    Fixed async problem with lazy loading.

--- src/lazy.js
+++ src/lazy.js
@@ -1,15 +1,22 @@
+const AsyncLock = require('async-lock')
+const { createUuid } = require('./utils')
+
 const lazyValue = method => {
+  const key = createUuid()
+  const lock = new AsyncLock()
   /* eslint-disable functional/no-let */
   let value = undefined
   let called = false
   return async (...args) => {
-    if (!called) {
-      called = true
-      value = await method(...args)
-      // eslint-disable-next-line require-atomic-updates
-    }
-
-    return value
+    return lock.acquire(key, async () => {
+      if (!called) {
+        called = true
+        value = await method(...args)
+        // eslint-disable-next-line require-atomic-updates
+      }
+    }).then(() => {
+      return value
+    })
   }
   /* eslint-enable functional/no-let */
 }
