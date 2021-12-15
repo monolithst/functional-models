d7bc0d359c2b9c3e99fb44fe542badb2bd0462ba
Mike Cornwell <michael.a.cornwell@gmail.com>
2021-12-14 21:57:56 -0500

    Updated

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import * as errors from './errors'
 import * as validation from './validation'
 import * as serialization from './serialization'
 import * as utils from './utils'
+import * as interfaces from './interfaces'
 
 export * from './models'
 export * from './properties'
 export * from './methods'
 
-export { constants, errors, validation, serialization, utils }
+export { constants, errors, validation, serialization, utils, interfaces }
